f28782791f918711aa79cda6f4823daeb5bc6578
Sami <38861758+sam-lopez@users.noreply.github.com>
2022-11-24 01:21:31 -0600

    Composite Keys on multivalue and relations added (contains,reviews,genres,langs). Unique constraint added to (directs,writes,works_for) then altered to be composite at end for award n:1 relationship to work

--- database_project3.sql
+++ database_project3.sql
@@ -22,36 +22,40 @@ Budget NUMBER(10)
 );
 
 CREATE TABLE Directs(
-dName VARCHAR2(40) CONSTRAINT Directs_dName_PK PRIMARY KEY,
+dName VARCHAR2(40) CONSTRAINT Directs_dName_uk UNIQUE,
 Movie_Title_Id VARCHAR2(20)
 );
 
 CREATE TABLE Writes(
-wName VARCHAR2(40) CONSTRAINT Writes_wName_PK PRIMARY KEY,
+wName VARCHAR2(40) CONSTRAINT Writes_wName_uk UNIQUE,
 Movie_Title_Id VARCHAR2(20)
 );
 
 CREATE TABLE Contains(
-Soundtrack_Title VARCHAR2(30) CONSTRAINT Contains_Soundtrack_Title_PK PRIMARY KEY,
-Movie_Title_Id VARCHAR2(20)
+Soundtrack_Title VARCHAR2(30),
+Movie_Title_Id VARCHAR2(20),
+CONSTRAINT Contains_Soundtrack_Title_PK PRIMARY KEY(Soundtrack_Title,Movie_Title_Id)
 );
 
 CREATE TABLE Reviews(
-Username VARCHAR2(25) CONSTRAINT Reviews_Username_PK PRIMARY KEY,
+Username VARCHAR2(25),
 Review_Contents VARCHAR2(100),
 Score VARCHAR2(20), 
 Date_of_Review DATE,
-Rev_Movie_Id VARCHAR2(20)
+Rev_Movie_Id VARCHAR2(20),
+CONSTRAINT Reviews_Username_PK PRIMARY KEY(Username,Rev_Movie_Id)
 );
 
 CREATE TABLE Genres(
-Genre VARCHAR2(20) CONSTRAINT Genres_Genre_PK PRIMARY KEY,
-Movie_Title_Id VARCHAR2(20)
+Genre VARCHAR2(20),
+Movie_Title_Id VARCHAR2(20),
+CONSTRAINT Genres_Genre_PK PRIMARY KEY(Genre,Movie_Title_Id)
 );
 
 CREATE TABLE Languages(
-Lang VARCHAR2(20) CONSTRAINT Languages_Lang_PK PRIMARY KEY,
-Movie_Title_Id VARCHAR2(20)
+Lang VARCHAR2(20),
+Movie_Title_Id VARCHAR2(20),
+CONSTRAINT Languages_Lang_PK PRIMARY KEY(Lang,Movie_Title_Id)
 );
 
 CREATE TABLE Award(
@@ -92,7 +96,7 @@ Ratings NUMBER(20)
 
 CREATE TABLE Works_For(
 Movie_Title_ID	VARCHAR2(20), 
-aName VARCHAR2(40) CONSTRAINT Works_For_aName_PK PRIMARY KEY,
+aName VARCHAR2(40) CONSTRAINT Works_For_aName_uk UNIQUE,
 Supporting VARCHAR2(20),
 Leading VARCHAR2(20),
 Pay NUMBER(20)
@@ -122,7 +126,7 @@ ALTER TABLE Works_For
     ADD CONSTRAINT Works_For_aName_fk FOREIGN KEY(aName)
     REFERENCES Person(Name);
 
-ALTER TABLE Contains 
+ALTER TABLE Contains
     ADD CONSTRAINT Contains_Movie_Title_Id_fk FOREIGN KEY(Movie_Title_Id)
     REFERENCES Movie(Movie_Title_Id);
 
@@ -155,9 +159,14 @@ ALTER TABLE Award
     ADD CONSTRAINT Award_Writer_Receives_fk FOREIGN KEY(Writer_Receives)
     REFERENCES Writes(wName);
 
+ALTER TABLE Works_For
+    ADD CONSTRAINT Works_For_aName_PK PRIMARY KEY(aName,Movie_Title_ID);
 
+ALTER TABLE Directs
+    ADD CONSTRAINT Directs_dName_PK PRIMARY KEY(dName,Movie_Title_Id);
 
-
+ALTER TABLE Writes
+    ADD CONSTRAINT Writes_wName_PK PRIMARY KEY(wName,Movie_Title_Id);
 
 DESC Movie;
 DESC Directs;
@@ -170,6 +179,3 @@ DESC Award;
 DESC Person;
 DESC Soundtrack;
 DESC Works_For;
-
-
-
