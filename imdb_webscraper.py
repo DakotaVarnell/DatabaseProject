3af851175b1662724a5f72d9a65d8c4033922e72
Ian Fields <IMFields03@gmail.com>
2022-11-30 17:46:53 -0600

    InsertFile method (doesn't exec automatically yet)

--- imdb_webscraper.py
+++ imdb_webscraper.py
@@ -1,19 +1,44 @@
 #Webscraper for the imdb website
 from imdb import Cinemagoer
+import cx_Oracle
 
 
-#iterates through every movie in the top 250 list
-#for id in (list_of_ids):
+def writeInsertFile(tableName, infoList):
+    testFile = open('insertFile.sql', 'a')
 
-# create an instance of the Cinemagoer class
+    query = "\ninsert into " + tableName + "\n" + "\t" + "values("
+
+    print("Still Running\n")
+
+    for attribute in infoList:
+        if (type(attribute) == int):
+            query+= str(attribute) + ","
+        elif (type(attribute) == float):
+            query+= str(attribute) + ","
+        else:
+            query+= "\'" + attribute + "\'" + ","
+    
+    query = query.rstrip(query[-1])
+    query += ');'
+
+    testFile.write(query)
+
+def write_movies(table_name, id):
+    movie = ia.get_movie(id)
+
+    try:
+        revenue = int(str(movie['box office']['Cumulative Worldwide Gross']).split()[0].replace(',', '').replace('$', '').replace('}', '').replace("'", ''))
+        budget = int(str(movie['box office']['Budget']).split()[0].replace(',', '').replace('$', '').replace('}', '').replace("'", ''))
+    except:
+        revenue = ''
+        budget = ''
+
+
+    info = [id, movie['title'], '', movie['rating'], str(movie['certification'][-4]).split(':')[1], revenue, budget]
+    writeInsertFile('Movie', info)
+
 ia = Cinemagoer()
-
-#Top 250 movies list
 list_of_250 = ia.get_top250_movies()
 
-
-#Update the keys of the movie to include these categories, not included by default
-ia.update(list_of_250, ['reviews'])
-ia.update(list_of_250, ['awards'])
-ia.update(list_of_250, ['soundtrack'])
-
+for movie in list_of_250:
+    write_movies(0, movie.movieID)
