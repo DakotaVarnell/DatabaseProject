cf04dc8420aa8ffa2be4db6ea56f46c7c248b19d
Ian Fields <IMFields03@gmail.com>
2022-12-07 21:40:23 -0600

    Update database_project.py
    Added get num_of_movies function

--- database_project.py
+++ database_project.py
@@ -1,6 +1,11 @@
 #Webscraper for the imdb website
 from imdb import Cinemagoer
 import cx_Oracle
+from bs4 import BeautifulSoup
+import requests
+import lxml
+import pandas as pd
+import re
 actor_IDs = set()
 director_IDs = set()
 writer_IDs = set()
@@ -199,15 +204,10 @@ def write_person(table_name, mID):
             hometown = person['birth notes']        
         except:
             hometown = ''        
-        try:
-            number_of_movies=0
-            films = ia.get_person_filmography(id)
-            for film in films['data']['filmography']['actor']:
-                number_of_movies+=1
-            print('num films',number_of_movies)
-        except:
-            number_of_movies = 1
-            print('num movies',number_of_movies)
+        
+
+        number_of_movies = 0
+        gen_num_movies(id)
 
         aFlag, wFlag, dFlag = '', '', ''
         if id in actor_IDs:
@@ -280,6 +280,40 @@ def write_awards(table_name, id):
     except:
             awards = [str(id), '','','','','']
             writeInsertFile('Award', awards)
+
+def get_num_movies(id):
+    url = "https://www.imdb.com/name/nm"
+    hdr = {'User-Agent': 'Mozilla/5.0'}
+
+    url = url + str(id) + "/"
+
+    req = requests.get(url, headers=hdr).content
+
+    soup = BeautifulSoup(req, "lxml")
+
+    liPerformances = soup.find_all('li', {"class": "ipc-inline-list__item credits-total"})
+
+    x = str(liPerformances)
+    performanceNumbers = []
+
+    #Splits on ">" character, then checks if the immediate character afterwards is a number
+    for number in x.split(">"):
+        if number[0].isdigit():
+            performanceNumbers.append(number)
+
+    performanceCount = []
+
+    #Takes off '<\li' 
+    for number in performanceNumbers:
+        size = len(number)
+        performanceCount.append(int(number[:size - 4]))
+
+    total = 0
+
+    for x in performanceCount:
+        total += x
+
+    return(x)
 
 # create an instance of the Cinemagoer class
 ia = Cinemagoer()
