61d3e06910e83c9400c945df0b29046d7c8ce77c
Dakota <89564744+DakotaVarnell@users.noreply.github.com>
2022-12-08 00:38:46 -0600

    added Ian's code

--- queries.sql
+++ queries.sql
@@ -4,15 +4,32 @@
 --Get all movies with the highest ratings
 SELECT * from movie where rating > 9;
 
---2, 3 Design 2 meaningful queries that join two tables;
+--2 Design 2 meaningful queries that join two tables
 SELECT Movie.Movie_Name, Writes.wName
 FROM Movie
 INNER JOIN Writes ON Movie.Movie_Title_Id=Writes.Movie_Title_Id;
 
---4, 5 Design 2 meaningful queries that join three tables;
+--3 Design 2 meaningful queries that join two tables
+
+
+--4 Design 2 meaningful queries that join three tables
+
+--5 Design 2 meaningful queries that join three tables
 
 --6 Design 1 meaningful query that joins four tables
 
---7, 8 Design 2 meaningful queries that use set theory (union, intersection or minus) operations;
+--7 Design 2 meaningful queries that use set theory (union, intersection or minus) operations
+SELECT dName FROM Directs WHERE Movie_Title_Id='0068646'
+UNION
+SELECT wName FROM Writes WHERE Movie_Title_Id='0068646';
 
---9, 10 Design 2 meaningful queries that use grouping operations
+--8 Design 2 meaningful queries that use set theory (union, intersection or minus) operations
+SELECT wName FROM Writes
+INTERSECT
+SELECT dName from Directs;
+
+--9 Design 2 meaningful queries that use grouping operations
+SELECT COUNT(Movie_Title_Id) from Movie;
+
+--10 Design 2 meaningful queries that use grouping operations
+SELECT AVG(budget) FROM movies;
