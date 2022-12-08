c09a55ce16ee12b4b97701bde3c2ebde83eb61b3
DakotaVarnell <jvarnell1@cub.uca.edu>
2022-12-07 20:32:53 -0600

    added numbers

--- queries.sql
+++ queries.sql
@@ -1,1 +1,17 @@
 --Where our Queries will go
+
+--1 Design 1 meaningful query use only one table
+--Get all movies with the highest ratings
+SELECT * from movie where rating > 9;
+
+--2, 3 Design 2 meaningful queries that join two tables;
+
+
+--4, 5 Design 2 meaningful queries that join three tables;
+
+--6 Design 1 meaningful query that joins four tables
+
+--7, 8 Design 2 meaningful queries that use set theory (union, intersection or minus) operations;
+
+--9, 10 Design 2 meaningful queries that use grouping operations
+
